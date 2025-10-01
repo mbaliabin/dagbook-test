71555edd57dae451683f4962214bb70ea9f56af2
baliabin.maksim <baliabin.maksim@gmail.com>
2025-10-01 22:56:52 +0300

    Изменения ProfilePage.tsx 15

--- fasx-frontend/src/pages/ProfilePage.tsx
+++ fasx-frontend/src/pages/ProfilePage.tsx
@@ -171,7 +171,7 @@ export default function ProfilePage() {
 
   return (
     <div className="min-h-screen bg-[#0e0e10] text-white px-4 py-6">
-      <div className="max-w-7xl mx-auto space-y-6">
+      <div className="max-w-7xl mx-auto space-y-6 pb-24">
         {/* Верхнее меню */}
         <div className="flex justify-around bg-[#1a1a1d] border-b border-gray-700 py-2 px-4 rounded-xl">
           {menuItems.map((item) => {
