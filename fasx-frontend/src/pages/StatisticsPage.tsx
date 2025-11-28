309cf26a2155d60c5566c1234ee008dc214ff933
baliabin.maksim <baliabin.maksim@gmail.com>
2025-11-28 21:17:20 +0300

    Доработка StatisticsPage.tsx 394

--- fasx-frontend/src/pages/StatisticsPage.tsx
+++ fasx-frontend/src/pages/StatisticsPage.tsx
@@ -196,7 +196,7 @@ const CustomTooltip: React.FC<any> = ({ active, payload, label, formatHours }) =
   };
 
 const TableSection: React.FC<{ table: any; index: number }> = ({ table, index }) => {
-  const colWidth = 100;
+  const colWidth = 103;
   const leftWidth = 200;
   const totalWidth = 80;
 
